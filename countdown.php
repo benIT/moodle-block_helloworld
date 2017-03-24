f3538ff84ebaab5a16fafa42a492d32a9a908f23
ben <benoit.works@gmail.com>
2017-03-24 15:54:46 +0100

    create a countdown page with renderer, template, css, external js

--- countdown.php
+++ countdown.php
@@ -1,18 +1,45 @@
 <?php
-// Standard GPL and phpdocs
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * countdownpage
+ * @package     block
+ * @subpackage  hello_world
+ * @copyright   2017 benIT
+ * @author      benIT <benoit.works@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 require_once(__DIR__ . '/../../config.php');
-require_once($CFG->libdir.'/adminlib.php');
-//admin_externalpage_setup('tooldemo');
+require_once($CFG->libdir . '/adminlib.php');
+
 // Set up the page.
 $title = get_string('pluginname', 'block_helloworld');
+$PAGE->requires->js('/blocks/helloworld/js/jquery-3.2.0.min.js', true); //loaded in begining
+$PAGE->requires->js('/blocks/helloworld/js/countdown.js', false); //loaded at the end
+$PAGE->set_pagelayout("standart");
 $pagetitle = $title;
 $url = new moodle_url("/blocks/helloworld/countdown.php");
 $PAGE->set_url($url);
 $PAGE->set_title($title);
-$PAGE->set_heading("Welcome to a page using a template");
 $output = $PAGE->get_renderer('block_helloworld');
 echo $output->header();
 echo $output->heading();
-$renderable = new countdown_page('Hello guys !');
+$data = new stdClass();
+$data->heading = 'Countdown page';
+$data->descriptive = 'This countdown uses external javascript and a mustache template.';
+$data->credit = "http://codepen.io/mattlitzinger/pen/ysowF";
+$renderable = new countdown_page($data);
 echo $output->render($renderable);
 echo $output->footer();
---
+++ js/countdown.js
@@ -0,0 +1,32 @@
+var now = new Date();
+var target_date = now.getTime() + (40 * 60 * 1000); // set the countdown date
+var days, hours, minutes, seconds; // variables for time units
+var countdown = document.getElementById("tiles"); // get tag element
+
+var startDate = document.getElementById("start-date");
+startDate.innerHTML = "started at " + now;
+getCountdown();
+
+var id = setInterval(function () {
+    getCountdown();
+}, 1000);
+
+function getCountdown() {
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+    if (seconds_left <= 0) {
+        clearInterval(id);
+    }
+    days = pad(parseInt(seconds_left / 86400));
+    seconds_left = seconds_left % 86400;
+    hours = pad(parseInt(seconds_left / 3600));
+    seconds_left = seconds_left % 3600;
+    minutes = pad(parseInt(seconds_left / 60));
+    seconds = pad(parseInt(seconds_left % 60));
+    countdown.innerHTML = "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
+}
+
+function pad(n) {
+    return (n < 10 ? '0' : '') + n;
+}
